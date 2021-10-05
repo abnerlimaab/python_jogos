0d7f3290ec74d62b921162fa90c06a14095dcb90
abnerlimaab <abnerlima.1997@gmail.com>
2021-10-05 18:29:39 -0400

    A Sequencia do jogo

---
+++ README.md
@@ -0,0 +1,28 @@
+## Introdução ao Python 3
+
+### Estruturas de repetição:
+
+**While**
+
+```python
+while(condicao):
+    #Código
+```
+### Estruturas condicionais
+
+**if**
+
+```python
+if(condicao):
+    #Código
+elif(condicao):
+    #Código
+else:
+    #Código
+```
+
+### Interpolação de String
+
+```python
+print("texto {} texto {}".format(variavel1, variavel2))
+```
--- advinhacao.py
+++ advinhacao.py
@@ -3,19 +3,27 @@ print("Bem-vindo ao jogo da advinhação")
 print("===============================")
 
 numero_secreto = 42
+total_de_tentativas = 3
+rodada = 1
 
-chute = int(input("Digite seu número: "))
+while(rodada <= total_de_tentativas):
+    print("Tentativa {} de {}".format(rodada, total_de_tentativas))
+    chute = int(input("Digite seu número: "))
 
-print("Você digitou ", chute)
+    print("Você digitou ", chute)
 
-acertou = numero_secreto == chute
-maior = chute > numero_secreto
-menor = chute < numero_secreto
+    acertou = numero_secreto == chute
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
 
-if (acertou):
-    print("Você acertou!")
-else:
-    if(maior):
-        print("Seu chute foi maior que o número secreto")
-    elif(menor):
-        print("Seu chute foi menor que o número secreto")
+    if (acertou):
+        print("Você acertou!")
+    else:
+        if(maior):
+            print("Seu chute foi maior que o número secreto")
+        elif(menor):
+            print("Seu chute foi menor que o número secreto")
+
+    rodada = rodada + 1
+
+print("Fim do jogo!")
